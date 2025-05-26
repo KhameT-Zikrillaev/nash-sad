dc41f840067fcd0540ae50996713d0b2f25e0d64
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2025-05-26 17:02:55 +0500

    fixeded

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Section2 from "./home/components/section2";
 import Section3 from "./home/components/section3";
 import Section4 from "./home/components/section4";
 import NewsPage from "./home/components/news";
-import Section5 from "./home/components/section5";
+// import Section5 from "./home/components/section5";
 export default function Home() {
   return (
     <main className=" md:mt-[164px] mt-[80px] min-h-screen overflow-hidden">
@@ -12,11 +12,8 @@ export default function Home() {
       <Section2 />
       <Section3 />
       <NewsPage />
-      <Section5 />
+      {/* <Section5 /> */}
       <Section4 />
-  
- 
-    
     </main>
   )
 }
